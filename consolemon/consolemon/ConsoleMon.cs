5a3c477a5e60e4c9d55349fc3a203757538ec7e2
jan van foreest <30220@ma-web.nl>
2022-05-20 11:41:18 +0200

    consolemon-commit2

---
+++ consolemon/consolemon/ConsoleMon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolemon
+{
+    class ConsoleMon
+    {
+        private string name;
+        private int health;
+        private int energy;
+
+
+        internal ConsoleMon()
+        {
+            name = "gert";
+            health = 210;
+            energy = 70;
+        }
+    }
+}
---
+++ consolemon/consolemon/skill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolemon
+{
+    class skill
+    {
+        private int damage;
+        private int energycost;
+        private string name;
+
+        internal skill()
+        {
+            name = "slaan";
+            damage = 15;
+            energycost = 15;
+        }
+    }
+}
